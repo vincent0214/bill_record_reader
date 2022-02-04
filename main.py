5f6622e852b77ffba7f56257163f8183aa546799
vincent <vincent0214@qq.com>
2022-02-04 20:08:05 +0800

    增加清空临时目录操作

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import imp
 import os
 import pandas as pd
-from util.FileUtil import FileUtil
-from util.PandasUtil import PandasUtil
-
+from util.file_util import FileUtil
+from util.pandas_util import PandasUtil
+from alipay_record_reader import read_alipay_record
+from wx_record_reader import read_wx_record
+import shutil
 
 def make_target_dir():
     if not os.path.exists("./target"):
@@ -23,9 +25,11 @@ def get_tables(files):
             tables.append(table)
     return tables
 
-
-make_target_dir()
-files = FileUtil.scan_file(r"./temp/")
+shutil.rmtree("./temp")
+make_temp_dir()
+read_alipay_record()
+read_wx_record()
+files = FileUtil.scan_file(r"./temp")
 tables = get_tables(files)
 result = PandasUtil.merge_tables(tables)
 result.to_excel("./target/2021-收支表.xlsx", index=False, sheet_name="收支")
