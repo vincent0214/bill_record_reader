61efa2fa5ea3ee4eccd8ca26db1be82cf46e3ef7
vincent <vincent0214@qq.com>
2022-02-04 14:44:44 +0800

    fix bug

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def make_temp_dir():
 def get_tables(files):
     tables = []
     for file in files:
-        if file.endswith("xlsx"):
-            table = pd.read_excel(file)
+        if file.name.endswith("xlsx"):
+            table = pd.read_excel(file.path)
             tables.append(table)
     return tables
 
