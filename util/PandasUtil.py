eb27e2c0efd2a566911c701ea982b8e603668645
vincent <vincent0214@qq.com>
2022-02-04 14:43:17 +0800

    补充注释

--- util/PandasUtil.py
+++ util/PandasUtil.py
@@ -7,6 +7,9 @@ import pandas as pd
 class PandasUtil:
     @staticmethod
     def merge_tables(tables):
+        """
+        合并表格(相同格式,上下合并)
+        """
         result = None
         for table in tables:
             if result is None:
