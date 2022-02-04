630095e5e662baba6a7dc80f2215f707fdef143c
vincent <vincent0214@qq.com>
2022-02-04 10:40:26 +0800

    取消按时间排序,增加"来源"字段

--- merge/main.py
+++ merge/main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
 
-table_1 = pd.read_excel("./2021年-支付宝.xlsx")
+table_1 = pd.read_excel("./2021年支付宝收支表.xlsx")
 table_2 = pd.read_excel("./2021年微信收支表.xlsx")
+table_1["来源"] = "支付宝"
+table_2["来源"] = "微信"
 table = table_1.append(table_2, ignore_index=True).reset_index(drop=True)
-table.sort_values("交易时间", inplace=True)
+# table.sort_values("交易时间", inplace=True)
 table.to_excel("./2021-收支表.xlsx", index=False, sheet_name="收支")
