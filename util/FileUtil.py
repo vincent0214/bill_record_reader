e4aba50aa6552ac547701a3ced794221e00ae7b5
vincent <vincent0214@qq.com>
2022-02-04 12:25:14 +0800

    增加工具类

---
+++ util/FileUtil.py
@@ -0,0 +1,20 @@
+import imp
+
+
+import os
+
+
+class FileUtil:
+    def scan_file(path):
+        files = []
+
+        def _scan_file(path):
+            for file_name in os.listdir(path):
+                file_path = path + "/" + file_name
+                if os.path.isdir(file_path):
+                    _scan_file(file_path)
+                else:
+                    files.append(file_path)
+
+        _scan_file(path)
+        return files
---
+++ util/PandasUtil.py
@@ -0,0 +1,15 @@
+import imp
+
+
+import pandas as pd
+
+
+class PandasUtil:
+    def merge_tables(tables):
+        result = None
+        for table in tables:
+            if result is None:
+                result = table
+            result = result.append(table, ignore_index=True)
+        result.reset_index(drop=True)
+        return result
