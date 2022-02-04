67239c2c6b48cff0c20cad3a77fd1ecd0d1a2a54
vincent <vincent0214@qq.com>
2022-02-04 12:08:58 +0800

    重构合并表格代码

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+
+def scan_file(path):
+    files = []
+
+    def _scan_file(path):
+        for i in os.listdir(path):
+            file_path = path + "/" + i
+            if os.path.isdir(file_path):
+                _scan_file(file_path)
+            else:
+                files.append(file_path)
+
+    _scan_file(path)
+    return files
+
+
+def get_tables(files):
+    tables = []
+    for file in files:
+        if file.endswith("xlsx"):
+            table = pd.read_excel(file)
+            tables.append(table)
+    return tables
+
+
+def merge(tables):
+    result = None
+    for table in tables:
+        if result is None:
+            result = table
+        result = result.append(table, ignore_index=True)
+    result.reset_index(drop=True)
+    return result
+
+
+files = scan_file(r"./temp/")
+tables = get_tables(files)
+result = merge(tables)
+result.to_excel("./target/2021-收支表.xlsx", index=False, sheet_name="收支")
