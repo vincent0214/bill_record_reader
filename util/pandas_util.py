1d09630c40c52a8b73d9143dd372a49ad940e97a
vincent <vincent0214@qq.com>
2022-02-10 13:03:54 +0800

    增加函数移动列

--- util/pandas_util.py
+++ util/pandas_util.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 class PandasUtil:
     @staticmethod
     def merge_tables(tables):
@@ -14,3 +15,19 @@ class PandasUtil:
             result = result.append(table, ignore_index=True)
         result.reset_index(drop=True)
         return result
+
+    @staticmethod
+    def change_col_place(table, name, new_place, new_name=None):
+        """
+        移动列
+        table: 表格
+        name: 原列名
+        new_plack: 新位置
+        new_name: 新列名
+        """
+        val = table[name]
+        table.drop(labels=[name], axis=1, inplace=True)
+        if new_name is None:
+            table.insert(new_place, column=name, value=val)  # 插入列
+        else:
+            table.insert(new_place, column=new_name, value=val)  # 插入列
